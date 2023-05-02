b479de23b3b10778eb20c956d3fb3b8cc6023870
atillakalay <atilla.kalayy@gmail.com>
2023-05-02 18:17:46 +0300

    Reference Operations and Writing Entities course has been completed.

---
+++ source/repos/EasyCashIdentityProject/EasyCashIdentityProject.EntityLayer/Concrete/CustomerAccount.cs
@@ -0,0 +1,11 @@
+﻿namespace EasyCashIdentityProject.EntityLayer.Concrete
+{
+    public class CustomerAccount
+    {
+        public int CustomerAccountId { get; set; }
+        public string CustomerAccountNumber { get; set; }
+        public string CustomerAccountCurrency { get; set; }
+        public decimal CustomerAccountBalance { get; set; }
+        public string BankBranch { get; set; }
+    }
+}
---
+++ source/repos/EasyCashIdentityProject/EasyCashIdentityProject.EntityLayer/Concrete/CustomerAcountProcess.cs
@@ -0,0 +1,10 @@
+﻿namespace EasyCashIdentityProject.EntityLayer.Concrete
+{
+    public class CustomerAcountProcess
+    {
+        public int CustomerAccountProcessId { get; set; }
+        public string ProcessType { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime PrecessDate { get; set; }
+    }
+}
