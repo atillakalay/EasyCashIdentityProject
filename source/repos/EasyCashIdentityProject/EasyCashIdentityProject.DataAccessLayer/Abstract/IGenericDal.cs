d8f9c4f1613e3cb79412fdfc8b4e130e58aa3f79
atillakalay <atilla.kalayy@gmail.com>
2023-05-09 18:36:14 +0300

    Interface and Writing Repositories course has been completed.

---
+++ source/repos/EasyCashIdentityProject/EasyCashIdentityProject.DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,11 @@
+﻿namespace EasyCashIdentityProject.DataAccessLayer.Abstract
+{
+    public interface IGenericDal<T> where T : class
+    {
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        T GetById(int id);
+        List<T> GetAll();
+    }
+}
