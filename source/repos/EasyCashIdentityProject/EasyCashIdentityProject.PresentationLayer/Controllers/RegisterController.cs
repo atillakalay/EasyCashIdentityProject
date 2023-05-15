9e40bb329d092037c1ebfdeae6d7031266d3938c
atillakalay <atilla.kalayy@gmail.com>
2023-05-15 15:38:47 +0300

    The course of Sending Confirmation Code to Mail has been completed.

--- source/repos/EasyCashIdentityProject/EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController.cs
+++ source/repos/EasyCashIdentityProject/EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController.cs
@@ -1,7 +1,9 @@
 ﻿using EasyCashIdentityProject.DtoLayer.Dtos.AppUserDtos;
 using EasyCashIdentityProject.EntityLayer.Concrete;
+using MailKit.Net.Smtp;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using MimeKit;
 
 namespace EasyCashIdentityProject.PresentationLayer.Controllers
 {
@@ -22,25 +24,43 @@ namespace EasyCashIdentityProject.PresentationLayer.Controllers
         [HttpPost]
         public async Task<IActionResult> Index(AppUserRegisterDtos appUserRegisterDtos)
         {
-            if (ModelState.IsValid)
-            {
-                Random random = new Random();
-                AppUser appUser = new AppUser()
-                {
-                    UserName = appUserRegisterDtos.UserName,
-                    Email = appUserRegisterDtos.Email,
-                    Name = appUserRegisterDtos.Name,
-                    LastName = appUserRegisterDtos.Surname,
-                    City = "aaaa",
-                    ImageUrl = "cccc",
-                    ConfirmCode = random.Next(100000, 1000000).ToString()
-                };
-                var result = await _userManager.CreateAsync(appUser, appUserRegisterDtos.Password);
+            if (!ModelState.IsValid)
+                return View(appUserRegisterDtos);
 
-                if (result.Succeeded) { return RedirectToAction("Index", "ConfirmEmail"); }
+            var random = new Random();
+            var appUser = new AppUser
+            {
+                UserName = appUserRegisterDtos.UserName,
+                Email = appUserRegisterDtos.Email,
+                Name = appUserRegisterDtos.Name,
+                LastName = appUserRegisterDtos.Surname,
+                City = "aaaa",
+                ImageUrl = "cccc",
+                ConfirmCode = random.Next(100000, 1000000).ToString()
+            };
+
+            var result = await _userManager.CreateAsync(appUser, appUserRegisterDtos.Password);
+            if (!result.Succeeded)
+                return View(appUserRegisterDtos);
+
+            var mimeMessage = new MimeMessage();
+            mimeMessage.From.Add(new MailboxAddress("Easy Cash Admin", "atilla.kalayy@gmail.com"));
+            mimeMessage.To.Add(new MailboxAddress("User", appUser.Email));
+            mimeMessage.Subject = "Easy Cash Onay Kodu";
+
+            var bodyBuilder = new BodyBuilder();
+            bodyBuilder.TextBody = $"Kayıt işlemini gerçekleştirmek için onay kodunuz: {appUser.ConfirmCode}";
+            mimeMessage.Body = bodyBuilder.ToMessageBody();
+
+            using (var smtpClient = new SmtpClient())
+            {
+                smtpClient.Connect("smtp.gmail.com", 587, false);
+                smtpClient.Authenticate("atilla.kalayy@gmail.com", "ofuvsooeyqshwfow\r\n");
+                smtpClient.Send(mimeMessage);
+                smtpClient.Disconnect(true);
             }
 
-            return View(appUserRegisterDtos);
+            return RedirectToAction("Index", "ConfirmEmail");
         }
     }
 }
